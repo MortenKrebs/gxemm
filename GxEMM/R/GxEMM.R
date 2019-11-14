a45a7a23b777761b3ffe06b4c0a98cdb4622453a
MortenKrebs <mortendkrebs@gmail.com>
2019-11-14 09:25:11 -0800

    Update GxEMM.R
    If lowmem==T write kinship-matices without loading them into R.

--- GxEMM/R/GxEMM.R
+++ GxEMM/R/GxEMM.R
@@ -41,6 +41,7 @@ GxEMM	<- function(
 		stop( 'cbind(1,X) is nearly collinear; either X should be scaled or a column should be dropped' )
 
 	### write each of the kinship matrices
+	if(lowmem==F){
 	ws	<- write_kin( tmpdir, K, index=1, ldak_loc, X ) # hom K
 	if( gtype == 'iid' ){
 		ws	<- c( ws, write_kin( tmpdir, K * (Z %*% t(Z)), index=2, ldak_loc, X ) ) # iid K
@@ -52,6 +53,78 @@ GxEMM	<- function(
 	if( etype == 'free' )
 		for( k in 1:ifelse( binary, K0, K0-1 ) ) #K0-th is omitted because singular with Hom noise for categorical Z. In simulations with binary traits, however, the background seems to play a different role and remains identified for categorical Z, so I use all K0 K*E matrices in the Free mode. This is very heuristic at this stage, but the simulation results are consistent across many simulation settings.
 			ws	<- c( ws, write_kin( tmpdir, diag( Z[,k]^2 ), index=length(ws)+1, ldak_loc, X ) ) # I * (Zk Zk^T)
+	} else {
+  
+    if(is.matrix(K)) { 
+      N= length(y)
+      prefix  <- paste0( tmpdir, '/K.', 0 )
+      keep = which( lower.tri(K,diag=T) , arr.ind=T )
+      non_mis = rep( 1  , length(keep[,1]) )
+      keep = cbind( keep , non_mis , K[ keep ] )
+      keep = keep[ order(keep[,1],keep[,2]) , ]
+      write.table( keep                 , file=paste0( prefix, '.grm' ), col.names=F, row.names=F, quote=F )
+      write.table( keep                     , file=gzfile(paste0( prefix, '.grm.gz' )), col.names=F, row.names=F, quote=F )
+      write.table( cbind(1:N,1:N)   , file=       paste0( prefix, '.grm.id' ) , col.names=F, row.names=F, quote=F )
+      #system( paste0( ldak_loc, ' --convert-gz ', prefix, ' --grm ', paste0(prefix) ) )
+    K = prefix
+    }
+    #Hom g
+    ws <- 1 
+    N= length(y)
+    prefix  <- paste0( tmpdir, '/K.', 1) 
+    system( paste0( ldak_loc, ' --convert-gz ', prefix, ' --grm ', K ) )
+    write.table( cbind(1:N,1:N)  , file=       paste0( prefix, '.grm.id' ) , col.names=F, row.names=F, quote=F )
+    # Het g
+    if( gtype %in% c('iid','free') | etype=='free' ){
+      N= length(y)
+      #K = "HetSCZ/kinship/full"
+      #K = "Test/_tmp.Z2"
+      #tmpdir = "Test/"
+      write.table(cbind(1:nrow(Z),Z), file=       paste0( tmpdir, '/tmp.Z' ) , col.names=F, row.names=F, quote=F )
+      
+      system(paste0("awk '{ print $1, $2, $3, $4 }' ", K, ".grm",
+                    " > " , tmpdir, "/tmp.ZK0" ))
+      system(paste0("awk 'NR==FNR{A[$1]=$0;next}$2=A[$2]' ",  tmpdir, '/tmp.Z ',  tmpdir, "/tmp.ZK0",
+                    " > " , tmpdir, "/tmp.ZK1" ))
+      system(paste0("awk 'NR==FNR{A[$1]=$0;next}$1=A[$1]' ",  tmpdir, '/tmp.Z ', tmpdir, "/tmp.ZK1", 
+                    " > " , tmpdir, "/tmp.ZK2" ))
+      #k <- ncol(Z)
+      if( gtype == 'iid'){
+      mult <- paste0(paste0(" $", 1+1:K0, " * $",2+K0+1:K0), collapse=" + " )
+
+      system(paste0("awk '{ print $1, ", paste0(" $",c( K0+2, 2*K0+3),",",collapse=""),"( ", mult," ) * $",2*K0+4, " }' ", tmpdir, "/tmp.ZK2 > ", tmpdir, "/tmp.ZK3.grm"))
+      system(paste0("gzip ", tmpdir, "/tmp.ZK3.grm"))
+      write.table( cbind(1:N,1:N)  , file=       paste0( tmpdir, '/tmp.ZK3.grm.id' ) , col.names=F, row.names=F, quote=F )
+      prefix  <- paste0( tmpdir, '/K.', 2) 
+      ws <- c(ws, 1)
+      system( paste0( ldak_loc, ' --convert-gz ', prefix, ' --grm ', paste0(tmpdir, "/tmp.ZK3") ) )
+      } else if(gtype=='free') {
+        for(i in 1:K0){
+        mult <- paste0(" $", 1+i, " * $",2+K0+i)
+        system(paste0("awk '{ print $1, ", paste0(" $",c( K0+2, 2*K0+3),",",collapse=""),"( ", mult," ) * $",2*K0+4, " }' ", tmpdir, "/tmp.ZK2 > ", tmpdir, "/tmp.ZK3_",i,".grm"))
+        system(paste0("gzip ", tmpdir, "/tmp.ZK3_",i,".grm"))
+        write.table( cbind(1:N,1:N)  , file=       paste0( tmpdir, '/tmp.ZK3_',i,'.grm.id' ) , col.names=F, row.names=F, quote=F )
+        prefix  <- paste0( tmpdir, '/K.', 1+i) 
+        ws <- c(ws, mean(Z[,i]))
+        system( paste0( ldak_loc, ' --convert-gz ', prefix, ' --grm ', paste0(tmpdir, "/tmp.ZK3_",i) ) )
+      }}  
+      if(etype=='free'){
+        for( i in 1:ifelse( binary, K0, K0-1 ) ){  #K0-th is omitted because singular with Hom noise for categorical Z. In simulations with binary traits, however, the background seems to play a different role and remains identified for categorical Z, so I use all K0 K*E matrices in the Free mode. This is very heuristic at this stage, but the simulation results are consistent across many simulation settings.
+          # ind_1 == ind_2 * Z[,i]^2
+          ws <- c(ws, mean(Z[,i]))
+          prefix  <- paste0( tmpdir, '/K.', length(ws)) 
+          system(paste0("awk '{ print $1, ", paste0(" $",c( K0+2, 2*K0+3),",",collapse=""),
+                        "( $1 == $", K0+2,") * $",1+i,"^2}' ", tmpdir , "/tmp.ZK2 > ", tmpdir, "/tmp.ZK3_e",i,".grm"))
+          system(paste0("gzip ", tmpdir, "/tmp.ZK3_e",i,".grm"))
+          write.table( cbind(1:N,1:N)  , file=       paste0( tmpdir, '/tmp.ZK3_e',i,'.grm.id' ) , col.names=F, row.names=F, quote=F )
+          system( paste0( ldak_loc, ' --convert-gz ', prefix, ' --grm ', paste0(tmpdir, "/tmp.ZK3_e",i) ) )
+        }
+         
+      }
+      
+  } 
+  }
+		
 	rm(Z); gc()
 	r		<- length(ws)
 	ws	<- c( ws, 1 - ncol(X)/nrow(X) )
